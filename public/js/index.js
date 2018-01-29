97e46d546b3cf369c86ac1935f0af6f6cd2091c9
bcohen79 <bar.cohen@teamaol.com>
2018-01-29 11:47:32 +0200

    finish with the visitor counter

---
+++ public/js/index.js
@@ -0,0 +1,84 @@
+        // making a request from the client to the server to open up a webscoket,
+        // and keep the connection open
+        let socket = io(),
+        currentNumberOfVisitors = 0,
+        visitorsCounter = document.createElement('div');
+
+        visitorsCounter.className = 'visitorsCount';
+
+        socket.on('connect', () => {
+            console.log('connected to server');
+            createNumberElement();
+            document.body.appendChild(visitorsCounter);
+        });
+
+        //like when the server is down
+        socket.on('disconnect', () => {
+            console.log('disconnected from server')
+        });
+
+        socket.on('newVisitor', (visitorsCounter) => {
+            console.log('newVisitor', visitorsCounter);
+            setVisitorsCount(visitorsCounter);
+        });
+
+         socket.on('newAnonymous', ({ visitorsCounter: visitorsCounter, createdAt: createdAt}) => {
+            console.log(`newAnonymous at: ${ createdAt }`);
+            setVisitorsCount(visitorsCounter);
+        });
+
+
+        function keepMeAnonymous() {
+            socket.emit('anonymousRequest');
+            document.getElementById("anonymousButton").disabled = true;
+        }
+
+        function createNumberElement() {
+                let wrapper = document.createElement('div');
+                wrapper.className = 'wrapper';
+                wrapper.setAttribute('data-num', '0');
+
+                for (let j = 9; j >= 0; j--) {
+                    let number = document.createElement('div'),
+                    top = document.createElement('div'),
+                    bottom = document.createElement('div'),
+                    bottomContent = document.createElement('div');
+
+                    number.className = `n${j}`;
+                    top.className = 'top';
+                    top.innerHTML = j;
+
+                    bottom.className = 'bottom';
+                    bottom.appendChild(bottomContent);
+                    bottomContent.innerHTML = j;
+
+                    number.appendChild(top);
+                    number.appendChild(bottom);
+
+                    wrapper.appendChild(number);
+                }
+
+
+                visitorsCounter.insertBefore(wrapper, visitorsCounter.firstChild);
+        }
+
+        function setVisitorsCount(visitors) {
+            let digitsArray = visitors.toString().split('');
+
+            if (digitsArray.length > currentNumberOfVisitors.toString().split('').length) {
+                for (let i = 0; i < (digitsArray.length - currentNumberOfVisitors.toString().split('').length); i++) {
+                createNumberElement();
+            }
+        }
+        Array.from(visitorsCounter.children).forEach((element, index) => {
+            element.setAttribute('data-num', digitsArray[index]);
+        });
+        currentNumberOfVisitors = visitors;
+    }
+
+    function getGeoLocation() {
+        //function that activly get the cordinates of the user, first argument is sucess handler and the second is error handler
+        navigator.geolocation.getCurrentPosition((position) => {
+
+        })
+    }
--- server/server.js
+++ server/server.js
@@ -13,20 +13,36 @@ var server = http.createServer(app);
 //websocket server
 var io = socketIO(server);
 
+var visitorsCounter = 0;
+
 //middleware
 app.use(express.static(publicPath));
 
-//let you register an event listener
-//the most popular event is connection
+//let you register an event listener, the most popular event is connection
 io.on('connection', (socket) => {
-    console.log('new user connected');
+    visitorsCounter++;
+    console.log(`new user connected, visitors so far: ${ visitorsCounter }`);
 
+    // creating an event
+    io.emit('newVisitor', visitorsCounter);
 
     socket.on('disconnect', (reason) => {
         console.log(reason);
-  });
+    });
+
+    socket.on('anonymousRequest', () => {
+        visitorsCounter--;
+        console.log(`new anonymous request, decreasing visitors counter to ${ visitorsCounter }`);
+
+        io.emit('newAnonymous', {
+            visitorsCounter: visitorsCounter,
+            createdAt: new Date().getTime()
+        });
+    });
+
+
 });
 
 server.listen(port, () => {
     console.log(`server is up on port ${port}`);
-});
+}); 
